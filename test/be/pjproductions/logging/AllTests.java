99ac37634269aeae9b2c3741bce310c01b25fcc4
Peter Jans <pjiron@hotmail.com>
2013-03-15 22:33:43 +0100

    Trivial - Add TestClass template for further usage

---
+++ test/be/pjproductions/logging/AllTests.java
@@ -0,0 +1,11 @@
+package be.pjproductions.logging;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ELoggerTest.class })
+public class AllTests {
+
+}
